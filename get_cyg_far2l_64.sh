7f0e4b952bbd5469d833c2f2fc3588e495f2534d
unxed <unxed@mail.ru>
2019-12-01 05:01:46 +0300

    Update get_cyg_far2l_64.sh

--- get_cyg_far2l_64.sh
+++ get_cyg_far2l_64.sh
@@ -6,6 +6,6 @@ wget https://github.com/unxed/far2l-deb/raw/master/far2l_cyg_64.tgz
 tar -xzf far2l_cyg_64.tgz
 rm -rf far2l_cyg_64.tgz
 echo "#!/bin/bash" > /bin/far2l
-echo "ps | grep xinit || startxwin >/dev/null 2>&1 &" >> /bin/far2l
+echo "ps | grep xinit || startxwin >/dev/null 2>&1 & sleep 5" >> /bin/far2l
 echo "DISPLAY=:0.0 /opt/far2l/far2l" >> /bin/far2l
 chmod +x /bin/far2l
